0de0e516f09050ba99243540348e3efbca76e390
624395619 <624395619@qq.com>
2018-12-06 16:45:51 +0800

    统一依赖

---
+++ itoken-zipkin/src/main/java/com/mmx/itoken/zipkin/ZipKinApplication.java
@@ -0,0 +1,14 @@
+package com.mmx.itoken.zipkin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+
+    }
+}
